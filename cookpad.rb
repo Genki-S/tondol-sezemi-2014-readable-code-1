72d8ad483e2bb4de0dd0848370eeb7c34dbc2496
hosaka <hosaka@qoncept.co.jp>
2014-06-22 11:33:19 +0900

    spec5: 仕様を満足した

--- cookpad.rb
+++ cookpad.rb
@@ -9,7 +9,16 @@ else
   # spec3-
   filename = ARGV.shift
   recipe = File.read(filename)
+
+  # idとレシピを紐付けたmapを作る
+  id = 0
+  recipe_map = {}
   recipe.each_line {|line|
-    puts line
+  	recipe_map[id] = line
+  	id += 1
+  }
+
+  recipe_map.each_pair {|k, v|
+  	puts "#{k}: #{v}"
   }
 end
