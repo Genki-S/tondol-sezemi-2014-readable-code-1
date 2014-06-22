352ce7f0614c09de25bd666b6e09175bacb75ac8
hosaka <hosaka@qoncept.co.jp>
2014-06-22 11:09:53 +0900

    spec3: 実装完了，how-to-runは未更新
    - spec1での挙動を保存している
    - 複数行のデータファイルには未対応

--- cookpad.rb
+++ cookpad.rb
@@ -1,4 +1,14 @@
 #!/usr/bin/env ruby
 #-*- coding: utf-8 -*-
 
-puts "オムライス"
+if ARGV.size == 0
+  # spec1
+  # 引数が空のときの挙動を保存しておく
+  puts "オムライス"
+else
+  # spec3-
+  # 入力が1行のレシピのみを含むと仮定する
+  filename = ARGV.shift
+  recipe = File.read(filename).chomp
+  puts recipe
+end
